b52277c52b6bc59cac792f567fe8e165128f5440
dmwilliams.tech@gmail.com <dmwilliams.tech@gmail.com>
2013-12-11 17:51:28 -0500

    Ugly code need to comment and re-org

--- rubynist.rb
+++ rubynist.rb
@@ -2,17 +2,18 @@ require 'json'
 require 'net/http'
 require 'active_support/core_ext'
 require 'mongo'
-require 'date'
-
 include Mongo
+
+class NewVuls
 
 connection = MongoClient.new("localhost")
 db = connection.db("vulnerabilities")
 coll = db["mostRecent"]
-s = Net::HTTP.get_response(URI.parse('http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-recent.xml')).body
+s = Net::HTTP.get_response(URI.parse('https://nvd.nist.gov/static/feeds/xml/cve/nvdcve-2.0-modified.xml')).body
 pg = JSON.pretty_generate(Hash.from_xml(s)["nvd"])
 cursor = coll.find()
 vuls = ActiveSupport::JSON.decode(pg)
+cveList = []
 if not cursor.next_document
   vuls['entry'].each {|entry| 
   coll.insert(entry)
@@ -22,13 +23,16 @@ else
   puts "Else"
   vuls["entry"].each { |entry|
   cve = entry["id"]
+  cveList.push(cve)
+  next
+  
   cursor.each { |doc|
-    if cve != "#{doc ['id']}" 
+    if cveList.include? "#{doc ['id']}" 
       coll.update({"id" => "#{doc['id']}"}, entry, :upsert=> true )
       puts "New field"
       next
       
-    elsif cve == "#{doc ['id']}" && "#{doc['last_modified_datetime']}".to_date != entry['last_modified_datetime'].to_date
+    elsif not cveList.include? "#{doc ['id']}" && "#{doc['last_modified_datetime']}".to_date != entry['last_modified_datetime'].to_date
       coll.update({"id" => "#{doc ['id']}"}, entry)
       puts "Already existing"
       next
@@ -43,7 +47,8 @@ else
         Vulnerable_software_list: #{doc['vulnerable_software_list']}
         #{doc['integrity_impact']} 
         Summary: #{doc['summary']}" }
-        next
-
-}
+        
+      }
+puts cveList
+end
 end
