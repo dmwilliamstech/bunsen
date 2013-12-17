7285486ed27092d9a4b5b8c8fcf1fd01051eef20
dmwilliams.tech@gmail.com <dmwilliams.tech@gmail.com>
2013-12-17 16:15:12 -0500

    TDD test

--- spec/app_test.rb
+++ spec/app_test.rb
@@ -5,82 +5,116 @@ describe "Nist Application" do
   it "should respond to GET /" do
     get '/'
     last_response.status.should == 302
+
   end
   
-  it "should return search results as JSON on the /search.json interface" do
-    
-    get '/search.json?q="google"'
-    last_response.status.should == 200
-    last_response.Content-Type.should == "application/json"
-    last_response.body == ""
-    
-  end
-  
-  describe "PDF functionality" do
-    it "should return results as a valid PDF on the /search.pdf interface" do
-      get '/search.pdf?q="google"'
+  describe "Return all Vulnerabilities" do
+    it "should display all vulnerabilities in sorted by alphabetically and by date_published" do
+      get '/vulnerabilities/'
       last_response.status.should == 200
+      #last_response.Content-Type.should == "application/json"
+      puts last_response
+    
     end
   end
+  
+  describe "Json functionality" do
+    it "should return search results as JSON on the /search.json interface" do
+      post '/search.json?q="google"'
+      last_response.status.should == 200
+      last_response.Content-Type.should == "application/json"
+      last_response.status.Content-Disposition.should == "attachment; filename=\"<q>.json\""
+    end
     
-  # describe "#find_document_by_v_id" do
-  #   it "find doc by id" do
-  #     connection = MongoClient.new("localhost")
-  #     db = connection.db("vulnerabilities")
-  #     coll = db["mostRecent"]
-  #     coll.find_one(:id => "CVE-2013-7009").to_json.should == Net::HTTP.get_response(URI.parse('http://localhost:4567/vulnerabilities/CVE-2013-7009')).body
-  #     
-  #   end
-  # end
-  # describe "#search_any" do
-  #   it "will should search all db fields and display results that contain(all) parameters/keywords" do
-  #     #I am entering the word "unspecificed" and a specific datetime(should return a single result)
-  #     textInput = "unspecified 2013-12-07T16:55:09.623-05:00"
-  #     connection = MongoClient.new("localhost")
-  #     db = connection.db("vulnerabilities")
-  #     coll = db["mostRecent"]
-  #     coll.ensure_index({"$**" => Mongo::TEXT})
-  #     db.command({:text => 'mostRecent' , :search => "\\" + textInput.split(' ').to_s + "\\"}).count().should >= 1
-  #   end
-  # end
-  # describe "return all db docs" do
-  #   connection = MongoClient.new("localhost")
-  #   db = connection.db("vulnerabilities")
-  #   coll = db["mostRecent"]
-  #   coll.find().to_json.should == Net::HTTP.get_response(URI.parse('http://localhost:4567/vulnerabilities/')).body
-  # end
-  # describe "when no params" do
-  # 
-  #   it "pdf generation" do
-  #     request = Net::HTTP.get_response(URI.parse('http://localhost:4567/search/results/pdf'))
-  #     request.code.should == "302"
-  #   end
-  #   it "json generation" do
-  #     request = Net::HTTP.get_response(URI.parse('http://localhost:4567/search/results/asJson'))
-  #     request.code.should == "302"
-  #   end
-  # end
-  # #This test fails
-  # describe "no docs found" do 
-  #   it "no results in db" do
-  #     textInput = "jjddjdj"
-  #     connection = MongoClient.new("localhost")
-  #     db = connection.db("vulnerabilities")
-  #     coll = db["mostRecent"]
-  #     coll.ensure_index({"$**" => Mongo::TEXT})
-  #     db.command({:text => 'mostRecent' , :search => "\\" + textInput.split(' ').to_s + "\\"}).count().should == 0
-  #   end
-  #   it "not results from search url" do
-  #     link = 'http://localhost:4567/search/results/?q=peekaboo'
-  #     page = Nokogiri::HTML(RestClient.get(link))
-  #     page.css('div#results table tr th').each do |el|
-  #       if not el.text
-  #         el.text.should == 0
-  #       else
-  #         el.text != 0
-  #       end
-  #     end
-  #   
-  #   end
-  # end
-end
+    it "should return all vulnerabilities as JSON on the /vulnerabilties.json interface" do
+      get '/vulnerabilities.json'
+      last_response.status.should == 200
+      last_response.Content-Type.should == "application/json"
+      last_response.status.Content-Disposition.should == "attachment; filename=\"vulnerabilities.json\""
+    end
+  
+    describe "PDF functionality" do
+      it "should return results as a valid PDF on the /search.pdf interface" do
+        post '/search.pdf?q="google"'
+        last_response.status.should == 200
+        last_response.status.Content-Type.should == "application/pdf"
+        last_response.status.Content-Disposition.should == "attachment; filename=\"<q>.pdf\""
+        last_response.body.should ==""
+      end
+    
+      it "should return all vulnerabilities as PDF on the /vulnerabilties.json interface" do
+        get '/vulnerabilities.pdf'
+        last_response.status.should == 200
+        last_response.status.Content-Type.should == "application/pdf"
+        last_response.status.Content-Disposition.should == "attachment; filename=\"allVulnerabilities.pdf\""
+        last_response.body.should ==""
+      end
+    end
+  
+    describe "Search Params" do
+      it "should return results of software name search query" do
+        get '/search/?q="linux"'
+        last_response.status.should == 200
+        last_response.body['results'].should be_a_kind_of(Array)
+        last_response.body['results'].should == ""
+      end
+    
+      it "should return results of software and version" do
+        get '/search/?q="chrome 31"'
+        last_response.status.should == 200
+        last_response.body['results'].should be_a_kind_of(Array)
+        last_response.body['results'].should == ""      
+      end
+    
+      it "should return results of vender name" do
+        get '/search/?q="microsoft"'
+        last_response.status.should == 200
+        last_response.body['results'].should be_a_kind_of(Array)
+        last_response.body['results'].should == ""      
+      end
+    
+      it "should return results of query of words in summary" do
+        get '/search/?q="remote attack"'
+        last_response.status.should == 200
+        last_response.body['results'].should be_a_kind_of(Array)
+        last_response.body['results'].should == ""
+      end
+    
+      it "should return results of query for 5 words" do
+        get '/search/?q="gain privileges via unspecified vectors"'
+        last_response.status.should == 200
+        last_response.body['results'].should be_a_kind_of(Array)
+        last_response.body['results'].should == ""
+      end
+      
+      it "should raise error and alert user when no params entered" do
+      get '/search/?q=""'
+      last_response.should raise_error
+    end
+    end
+  
+    describe "Limit results" do
+      it "should return results limited to 10" do
+        get '/search/?q="linux"'
+        last_response.status.should == 200
+        last_response.body['results'].should be_a_kind_of(Array)
+        last_response.body['results'].should == ""      
+      end
+    end
+
+    describe "Return individual Vulnerability" do
+      it "should display single vulnerability" do
+        get '/vulnerabilities/<id>'
+        last_response.status.should == 200
+        last_response.Content-Type.should == "application/json"
+        body = JSON.parse(last_response.body)
+        body.should include('')
+        puts last_response
+      end
+      it "should raise error when no cve-id entered" do
+        get '/vulnerabilities/'
+        last_response.should raise_error
+      end
+    end
+
+  end
