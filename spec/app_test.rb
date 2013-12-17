6befaa93449cb60ddaee39c92facaa365e578af3
Kit Plummer <kitplummer@gmail.com>
2013-12-17 10:29:37 -0700

    mo rspec skeleton

--- spec/app_test.rb
+++ spec/app_test.rb
@@ -1,61 +1,48 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
-## include Mongo
-
-# class MyAppTest < Test::Unit::TestCase
-#   include Rack::Test::Methods
-# 
-#   def app
-#     Sinatra::Application
-#   end
-# 
-#   
-#   def test_my_default_redirect
-#     get 'http://localhost:4567'
-#     follow_redirect!
-#     assert_equal 'http://localhost:4567/search/', last_request.url
-#     assert last_response.ok?
-#   end
-#   def test_my_params
-#     get 'http://localhost:4567/vulnerabilities/'
-#     assert last_response.ok?
-#   end
-#   
-#   def test_my_default
-#     get 'http://localhost:4567/search/'
-#     assert last_response.ok?
-#   end
-# 
-# 
-# end
-
-describe "My Sinatra Application" do
+describe "Nist Application" do
   
   it "should respond to GET /" do
     get '/'
     last_response.status.should == 302
   end
+  
+  it "should return search results as JSON on the /search.json interface" do
     
-  describe "#find_document_by_v_id" do
-    it "find doc by id" do
-      connection = MongoClient.new("localhost")
-      db = connection.db("vulnerabilities")
-      coll = db["mostRecent"]
-      coll.find_one(:id => "CVE-2013-7009").to_json.should == Net::HTTP.get_response(URI.parse('http://localhost:4567/vulnerabilities/CVE-2013-7009')).body
-      
+    get '/search.json?q="google"'
+    last_response.status.should == 200
+    last_response.Content-Type.should == "application/json"
+    last_response.body == ""
+    
+  end
+  
+  describe "PDF functionality" do
+    it "should return results as a valid PDF on the /search.pdf interface" do
+      get '/search.pdf?q="google"'
+      last_response.status.should == 200
     end
   end
-  describe "#search_any" do
-    it "will should search all db fields and display results that contain(all) parameters/keywords" do
-      #I am entering the word "unspecificed" and a specific datetime(should return a single result)
-      textInput = "unspecified 2013-12-07T16:55:09.623-05:00"
-      connection = MongoClient.new("localhost")
-      db = connection.db("vulnerabilities")
-      coll = db["mostRecent"]
-      coll.ensure_index({"$**" => Mongo::TEXT})
-      db.command({:text => 'mostRecent' , :search => "\\" + textInput.split(' ').to_s + "\\"}).count().should >= 1
-    end
-  end
+    
+  # describe "#find_document_by_v_id" do
+  #   it "find doc by id" do
+  #     connection = MongoClient.new("localhost")
+  #     db = connection.db("vulnerabilities")
+  #     coll = db["mostRecent"]
+  #     coll.find_one(:id => "CVE-2013-7009").to_json.should == Net::HTTP.get_response(URI.parse('http://localhost:4567/vulnerabilities/CVE-2013-7009')).body
+  #     
+  #   end
+  # end
+  # describe "#search_any" do
+  #   it "will should search all db fields and display results that contain(all) parameters/keywords" do
+  #     #I am entering the word "unspecificed" and a specific datetime(should return a single result)
+  #     textInput = "unspecified 2013-12-07T16:55:09.623-05:00"
+  #     connection = MongoClient.new("localhost")
+  #     db = connection.db("vulnerabilities")
+  #     coll = db["mostRecent"]
+  #     coll.ensure_index({"$**" => Mongo::TEXT})
+  #     db.command({:text => 'mostRecent' , :search => "\\" + textInput.split(' ').to_s + "\\"}).count().should >= 1
+  #   end
+  # end
   # describe "return all db docs" do
   #   connection = MongoClient.new("localhost")
   #   db = connection.db("vulnerabilities")
